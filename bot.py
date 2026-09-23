d487ddba40381c04d2943bfc0b7921d6a5c8d006
tomaa-dev <lajpanovatoma@gmail.com>
2026-09-23 20:11:41 +0300

    Initial commit

---
+++ bot.py
@@ -0,0 +1,47 @@
+import logging
+import os
+from dotenv import load_dotenv
+
+from telegram import Update, ForceReply
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
+)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, context: CallbackContext):
+    user = update.effective_user
+    update.message.reply_markdown_v2(
+        fr'Hi {user.mention_markdown_v2()}\!',
+        reply_markup=ForceReply(selective=True),
+    )
+
+def help_command(update: Update, context: CallbackContext):
+    update.message.reply_text('Help!')
+
+
+def echo(update: Update, context: CallbackContext):
+    update.message.reply_text(update.message.text)
+
+
+def main():
+    load_dotenv()
+    token = os.getenv('TG_BOT_TOKEN')
+    updater = Updater(token)
+
+    dispatcher = updater.dispatcher
+
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
